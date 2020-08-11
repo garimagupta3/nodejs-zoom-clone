fd9a61ad837627f638b3617b769019807a0eeab1
garimagupta3 <46891596+garimagupta3@users.noreply.github.com>
2020-08-11 19:36:43 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -18,7 +18,7 @@ navigator.mediaDevices.getUserMedia({
   myPeer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video');
-    console.log("check webhook slash 11111");
+    console.log("check webhook slash 111111");
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream)
     })
