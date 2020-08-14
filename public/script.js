2942d6820d56330be0d816b7b1e8db709e3eefbf
garimagupta3 <46891596+garimagupta3@users.noreply.github.com>
2020-08-14 17:04:51 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -18,6 +18,7 @@ navigator.mediaDevices.getUserMedia({
   myPeer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video')
+    console.log("new pr");
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream)
     })
